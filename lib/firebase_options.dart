8bc2c8f5972aa563b32d8e06285dcb6ba75d10f0
Malay Pandit <malay13pandit@gmail.com>
2025-09-26 15:03:41 +0530

    fix security issues

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -42,46 +42,46 @@ class DefaultFirebaseOptions {
   }
 
   static FirebaseOptions get web => FirebaseOptions(
-    apiKey: dotenv.env['FIREBASE_WEB_API_KEY'] ?? 'AIzaSyA7h2KAszT8qCx_HK13csiufjLtm14CpUc',
-    appId: dotenv.env['FIREBASE_WEB_APP_ID'] ?? '1:834971965063:web:abcdef',
-    messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID'] ?? '834971965063',
-    projectId: dotenv.env['FIREBASE_PROJECT_ID'] ?? 'video-calling-app-5c03b',
-    authDomain: dotenv.env['FIREBASE_AUTH_DOMAIN'] ?? 'video-calling-app-5c03b.firebaseapp.com',
-    storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET'] ?? 'video-calling-app-5c03b.firebasestorage.app',
-  );
+        apiKey: dotenv.env['FIREBASE_WEB_API_KEY'] ?? '',
+        appId: dotenv.env['FIREBASE_WEB_APP_ID'] ?? '',
+        messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID'] ?? '',
+        projectId: dotenv.env['FIREBASE_PROJECT_ID'] ?? '',
+        authDomain: dotenv.env['FIREBASE_AUTH_DOMAIN'] ?? 'v',
+        storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET'] ?? '',
+      );
 
   static FirebaseOptions get android => FirebaseOptions(
-    apiKey: dotenv.env['FIREBASE_ANDROID_API_KEY'] ?? 'AIzaSyA7h2KAszT8qCx_HK13csiufjLtm14CpUc',
-    appId: dotenv.env['FIREBASE_ANDROID_APP_ID'] ?? '1:834971965063:android:698810271009f59572ca22',
-    messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID'] ?? '834971965063',
-    projectId: dotenv.env['FIREBASE_PROJECT_ID'] ?? 'video-calling-app-5c03b',
-    storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET'] ?? 'video-calling-app-5c03b.firebasestorage.app',
-  );
+        apiKey: dotenv.env['FIREBASE_ANDROID_API_KEY'] ?? '',
+        appId: dotenv.env['FIREBASE_ANDROID_APP_ID'] ?? '',
+        messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID'] ?? '',
+        projectId: dotenv.env['FIREBASE_PROJECT_ID'] ?? '',
+        storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET'] ?? '',
+      );
 
   static FirebaseOptions get ios => FirebaseOptions(
-    apiKey: dotenv.env['FIREBASE_IOS_API_KEY'] ?? 'AIzaSyAbpgtzkrwvv8PCqP5UQhJpAZYwmJWCb4o',
-    appId: dotenv.env['FIREBASE_IOS_APP_ID'] ?? '1:834971965063:ios:0fea277f94c2a5bf72ca22',
-    messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID'] ?? '834971965063',
-    projectId: dotenv.env['FIREBASE_PROJECT_ID'] ?? 'video-calling-app-5c03b',
-    storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET'] ?? 'video-calling-app-5c03b.firebasestorage.app',
-    iosBundleId: dotenv.env['FIREBASE_IOS_BUNDLE_ID'] ?? 'com.videocall.videoCallApp',
-  );
+        apiKey: dotenv.env['FIREBASE_IOS_API_KEY'] ?? '',
+        appId: dotenv.env['FIREBASE_IOS_APP_ID'] ?? '',
+        messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID'] ?? '',
+        projectId: dotenv.env['FIREBASE_PROJECT_ID'] ?? '',
+        storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET'] ?? '',
+        iosBundleId: dotenv.env['FIREBASE_IOS_BUNDLE_ID'] ?? '',
+      );
 
   static FirebaseOptions get macos => FirebaseOptions(
-    apiKey: dotenv.env['FIREBASE_IOS_API_KEY'] ?? 'AIzaSyAbpgtzkrwvv8PCqP5UQhJpAZYwmJWCb4o',
-    appId: dotenv.env['FIREBASE_IOS_APP_ID'] ?? '1:834971965063:ios:0fea277f94c2a5bf72ca22',
-    messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID'] ?? '834971965063',
-    projectId: dotenv.env['FIREBASE_PROJECT_ID'] ?? 'video-calling-app-5c03b',
-    storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET'] ?? 'video-calling-app-5c03b.firebasestorage.app',
-    iosBundleId: dotenv.env['FIREBASE_IOS_BUNDLE_ID'] ?? 'com.videocall.videoCallApp',
-  );
+        apiKey: dotenv.env['FIREBASE_IOS_API_KEY'] ?? '',
+        appId: dotenv.env['FIREBASE_IOS_APP_ID'] ?? '',
+        messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID'] ?? '',
+        projectId: dotenv.env['FIREBASE_PROJECT_ID'] ?? '',
+        storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET'] ?? '',
+        iosBundleId: dotenv.env['FIREBASE_IOS_BUNDLE_ID'] ?? '',
+      );
 
   static FirebaseOptions get windows => FirebaseOptions(
-    apiKey: dotenv.env['FIREBASE_WEB_API_KEY'] ?? 'AIzaSyA7h2KAszT8qCx_HK13csiufjLtm14CpUc',
-    appId: dotenv.env['FIREBASE_WEB_APP_ID'] ?? '1:834971965063:web:abcdef',
-    messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID'] ?? '834971965063',
-    projectId: dotenv.env['FIREBASE_PROJECT_ID'] ?? 'video-calling-app-5c03b',
-    authDomain: dotenv.env['FIREBASE_AUTH_DOMAIN'] ?? 'video-calling-app-5c03b.firebaseapp.com',
-    storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET'] ?? 'video-calling-app-5c03b.firebasestorage.app',
-  );
+        apiKey: dotenv.env['FIREBASE_WEB_API_KEY'] ?? '',
+        appId: dotenv.env['FIREBASE_WEB_APP_ID'] ?? '',
+        messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID'] ?? '',
+        projectId: dotenv.env['FIREBASE_PROJECT_ID'] ?? '',
+        authDomain: dotenv.env['FIREBASE_AUTH_DOMAIN'] ?? '',
+        storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET'] ?? '',
+      );
 }
